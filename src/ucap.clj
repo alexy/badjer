153dba3761d7adbbd089099438290fd9d76b51c6
Alexy Khrabrov <deliverable@gmail.com>
2010-10-20 20:21:41 -0400

    record how we load dreps and dments, actually

--- src/ucap.clj
+++ src/ucap.clj
@@ -1,5 +1,8 @@
 (ns sc)
 
+(def dreps-path "/opt/data/twitter/tokyo/dreps.clb")
+(let [[a b] (load-graphs dreps-path)] (def dreps a) (def dments b))
+
 (def ustats (:ustats sgraph))
 (time (def ucap (->> ustats (map (fn [[user {:keys [soc]}]] [user soc])) (sort-by second >))))
 (def ucapday (map (fn [[user soc]] [user soc (->> user ustats :day)]) ucap))

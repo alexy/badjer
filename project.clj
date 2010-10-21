97d1251aaa4676abebc08a795c0403183c1e5cc0
Alexy Khrabrov <deliverable@gmail.com>
2010-10-20 20:28:53 -0400

    require instead of load-file, upgrade clojure-protobuf

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
     [joda-time "1.6"]
     [cupboard "1.0-SNAPSHOT"]
     [net.1978th/tokyocabinet "1.23"]
-    [clojure-protobuf "0.2.11-SNAPSHOT"]
+    [clojure-protobuf "0.3.0-SNAPSHOT"]
     [jiraph "0.1.3-SNAPSHOT"]
     [clj-json/clj-json "0.3.0-SNAPSHOT"]
     ]
   :dev-dependencies [
-    [clojure-protobuf "0.2.11-SNAPSHOT"]
+    [clojure-protobuf "0.3.0-SNAPSHOT"]
     ]
   :tasks [protobuf.tasks]
   :native-path "/opt/local/lib"
--- src/load_data.clj
+++ src/load_data.clj
@@ -1,8 +1,8 @@
 (ns sc)
 
-(load-file "src/tokyo-graph.clj")
-(load-file "src/socrun.clj")
-(load-file "src/invert.clj")
+(require 'tokyo-graph)
+(require 'socrun)
+(require 'invert)
 
 
 (defn load-graph [dreps-name]
